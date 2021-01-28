698ca54eb359e32507a1f9d94a053e38bafe669d
Nicolas Marquez <nicolas_marquez10@hotmail.com>
2021-01-28 17:57:25 -0300

    MiniProfile muestra imagen por defecto

--- src/profile/MiniProfile.tsx
+++ src/profile/MiniProfile.tsx
@@ -1,18 +1,63 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
+import {getCurrentUserAsObject} from "../user/userService"
 import "../styles.css"
 
-export default function MiniProfile(props: any) {
+interface MiniProfileProps{
+    profile: {
+        name: string,
+        phone: string,
+        address: string,
+        province: string,
+        picture: string,
+        posts: Array<any>,
+        user: string
+    }
+}
+
+export default  function MiniProfile(props: any) {
+    const [currentUser,setCurrentUser] = useState<any>()
+
+    const loadCurrentUser = ()=>{
+        setCurrentUser(getCurrentUserAsObject())
+    }
+
+    const renderEditButton = () =>{
+        if(currentUser?.id === props.profile?.user){
+            return (
+                <NavLink to="/profile">
+                    <span className="float-right btn">
+                        <img src="/assets/edit-icon.png" width="20px" alt="Editar"/>
+                    </span>
+                </NavLink>
+            )
+        } else return null
+    }
+
+    const renderProfile = ()=>{
+        if(props.profile?.name || props.profile?.address || props.profile?.phone){
+            return (
+            <div className="">
+                <h5 className="card-title">{props.profile?.name}</h5>
+                <h5 className="card-title">{props.profile?.address}</h5>
+                <h5 className="card-title">{props.profile?.phone}</h5>
+            </div>
+            )
+        } else {
+            return <h5>La información de este perfil no esta disponible</h5>
+        }
+    }
+    useEffect(()=>{
+        loadCurrentUser()
+        //es-ignore-next-line
+    },[])
+
     return (
         <div className="card mb-5 mx-auto" style={{width: "18rem"}}>
-            <img src={props.profile?props.profile.picture:null} className="card-img-top" alt="..." />
+            <img src={props.profile?.picture?props.profile.picture:"/assets/default_profile_image.jpg"} className="card-img-top" alt="..." />
             <div className="card-body">
-                <div className="">
-                    <h5 className="card-title">{props.profile?props.profile.name:null}</h5>
-                    <h5 className="card-title">{props.profile?props.profile.address:null}</h5>
-                    <h5 className="card-title">{props.profile?props.profile.phone:null}</h5>
-                </div>
-                <NavLink to="/profile"><span className="float-right btn"><img src="/assets/edit-icon.png" width="20px" alt="Editar"/></span></NavLink>
+                {renderProfile()}
+                {renderEditButton()}
             </div>
         </div>
     )
